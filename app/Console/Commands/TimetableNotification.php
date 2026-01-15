4cebe30bfab133835211e598bbacd70225e96f38
Sorel-Haller <hallersorel@gmail.com>
2026-01-15 11:31:04 +0200

    kahtemoodi

--- app/Console/Commands/TimetableNotification.php
+++ app/Console/Commands/TimetableNotification.php
@@ -53,6 +53,15 @@ class TimetableNotification extends Command
             $date = Carbon::parse(data_get($entry, 'date'))->locale('et');
             $dayName = $date->dayName;
 
+            $entries[$dayName]['date'] = $date->toDateString('d F Y');
+            $entries[$dayName]['day'] = [$dayName];
+
+            $entries[$dayName]=[
+                'date' => $date->toDateString('d F Y'),
+                'day' => [$dayName],
+                
+            ];
+
             $entries[$dayName][] = [
                 'name' => data_get($entry, 'nameEt'),
                 'start' => data_get($entry, 'timeStart'),
