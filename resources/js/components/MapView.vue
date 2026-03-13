61594cc871951f80999eda72ddf0ffb70069d824
Sorel-Haller <hallersorel@gmail.com>
2026-03-13 16:55:12 +0200

    Map markers

--- resources/js/components/MapView.vue
+++ resources/js/components/MapView.vue
@@ -1,26 +1,61 @@
-<script setup>
-import { ref, onMounted } from "vue"
-import L from "leaflet"
-import "leaflet/dist/leaflet.css"
+<script setup lang="ts">
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Form } from '@inertiajs/vue3';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import { onMounted, ref } from 'vue';
+import { Button } from '@/components/ui/button';
+import { Textarea } from '@/components/ui/textarea';
 
-const mapEl = ref(null)
+const mapEl = ref(null);
+const mapClick = (e: PointerEvent) => {
+    selectedLocation.value = { lat: e.latlng.lat, lng: e.latlng.lng };
+};
+
+const selectedLocation = ref<{ lat: number; lng: number }>();
 
 onMounted(() => {
-  const test = () => console.log('test');
+    const map = L.map(mapEl.value, {
+        zoomControl: true,
+    }).setView([59.4, 24.7], 8);
 
-  const map = L.map(mapEl.value, {
-    zoomControl: true
-  }).setView([59.4, 24.7], 8)
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-  L.tileLayer(
-    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  ).addTo(map)
-
-  map.on('click', test);
-})
-
+    map.on('click', mapClick);
+});
 </script>
 
 <template>
-  <div ref="mapEl" class="map h-full"></div>
+    <div ref="mapEl" class="map z-10 h-full"></div>
+
+    <Dialog :open="!!selectedLocation" @update:open="selectedLocation = undefined">
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle>Salvesta uus marker</DialogTitle>
+                <DialogDescription> Lisa nimi ja kirjeldus </DialogDescription>
+                <Form action="/" method="post">
+                    <div class="grid grid-cols-2 gap-4">
+                        <div class="col-span-2">
+                            <Label class="mb-1.5" for="name">Nimi</Label>
+                            <Input name="name"> </Input>
+                        </div>
+
+                        <div class="col-span-1.5">
+                            <Label class="mb-1.5" for="lat">Lat</Label>
+                            <Input id="lat" name="lat" disabled :default-value="selectedLocation?.lat" />
+                        </div>
+
+                        <div class="col-span-1.5">
+                            <Label class="mb-1.5" for="lng">Lng</Label>
+                            <Input id="lng" name="lng" disabled :default-value="selectedLocation?.lng" />
+                        </div>
+                        <Textarea name="description" class="col-span-2" />
+                        <Button type="submit">Salvesta</Button>
+                    </div>
+                </Form>
+            </DialogHeader>
+        </DialogContent>
+    </Dialog>
 </template>
