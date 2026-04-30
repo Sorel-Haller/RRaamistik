ab4bdf6886321b07a8d232933249b69acabea6a6
Sorel-Haller <hallersorel@gmail.com>
2026-04-30 16:21:46 +0300

    original appservice

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -18,8 +17,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot()
+    public function boot(): void
     {
-        \Illuminate\Support\Facades\URL::forceScheme('https');
+        //
     }
 }
